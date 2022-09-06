ca1f646eb3c88aba55d42ec3318c04f6f2691542
ElinaElya15 <99654862+ElinaElya15@users.noreply.github.com>
2022-09-06 15:49:55 +0300

    Delete HomeWork_6 directory

--- HomeWork_6/task_1/student_task/student.js
+++
@@ -1,9 +0,0 @@
-let yearOfBirth = prompt('Введіть рік народження учня');
-
-let ageStudent = 2022 - Number(yearOfBirth);
-
-document.write(`
- <div>
-        <p>Вік учня у 2022 році: ${ageStudent}</p>
-    </div>
-`);
--- HomeWork_6/task_2/weeks_task/weeks.js
+++
@@ -1,9 +0,0 @@
-let days = prompt('Введіть кількість днів')
-
-let weeks = Math.floor (Number(days) / 7);
-
-document.write(`
-    <div>
-        <p>У ${days} днях ${weeks}тижні</p>
-    </div>
-`)
